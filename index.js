4420e7ee25f991ed93cfb3ef0195cf7b9337a1e9
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-18 21:03:35 -0700

    Initial commit for @token-ring/kubernetes

---
+++ index.js
@@ -0,0 +1,8 @@
+export { default as KubernetesService } from "./KubernetesService.js";
+
+export * as tools from "./tools.js";
+
+export const name = "@token-ring/kubernetes";
+export const description = "Kubernetes resources integration";
+export const version = "0.1.0";
+
