cadf43a481ae005d5c27bb9dda7bd2bf46695c0e
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:38 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,22 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {ChatService} from "@tokenring-ai/chat";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import KubernetesService, {KubernetesServiceParamsSchema} from "./KubernetesService.ts";
+import packageJSON from './package.json' with {type: 'json'};
+import tools from "./tools.ts";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const config = app.getConfigSlice('kubernetes', KubernetesServiceParamsSchema.optional());
+    if (config) {
+      app.waitForService(ChatService, chatService =>
+        chatService.addTools(packageJSON.name, tools)
+      );
+      app.addServices(new KubernetesService(config));
+    }
+  }
+} as TokenRingPlugin;
